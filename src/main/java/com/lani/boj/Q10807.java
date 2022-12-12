9a7914ea1cb9c0b1e421920e12791003890eb621
Lani7 <appletree0272@gmail.com>
2022-12-12 22:04:52 +0900

    10807

---
+++ src/main/java/com/lani/boj/Q10807.java
@@ -0,0 +1,28 @@
+package com.lani.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q10807 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int v = Integer.parseInt(bf.readLine());
+        int cnt = 0;
+        for(int i : array) {
+            if(i == v)  cnt++;
+        }
+
+        System.out.println(cnt);
+        bf.close();
+    }
+}
