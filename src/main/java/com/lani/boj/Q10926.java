8659c675b9b5feb67dced62379e5f4139ac9f287
Lani7 <appletree0272@gmail.com>
2022-12-27 22:33:05 +0900

    10926

---
+++ src/main/java/com/lani/boj/Q10926.java
@@ -0,0 +1,17 @@
+package com.lani.boj;
+
+import java.util.Scanner;
+
+public class Q10926 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String existedId = scanner.nextLine();
+
+        System.out.println(existedId + "??!");
+
+        scanner.close();
+
+    }
+}
