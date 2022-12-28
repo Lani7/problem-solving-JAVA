6f7be2629205e14716d0738671f409c8973d9d59
Lani7 <appletree0272@gmail.com>
2022-12-28 19:43:26 +0900

    1065

--- src/main/java/com/lani/boj/Q1065.java
+++ src/main/java/com/lani/boj/Q1065.java
@@ -37,10 +37,41 @@ public class Q1065 {
         return cnt;
     }
 
+    public static int hansu2(int n) {
+        int cnt = 0;
+        for(int i = 1; i <= n; i++) {
+            if(i < 100)   cnt++;
+            else {
+                int hun = i / 100;
+                int ten = (i % 100) / 10;
+                int one = i % 10;
+                if((hun - ten) == (ten - one)) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+
+    public static int hansu3(int n) {
+        int cnt = 0;
+        for(int i = 1; i <= n; i++) {
+            if(i < 100)   cnt++;
+            else {
+                String[] str = Integer.toString(i).split("");
+                if(Integer.parseInt(str[0])-Integer.parseInt(str[1]) == Integer.parseInt(str[1])-Integer.parseInt(str[2]))
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
         System.out.println( hansu(n) );
+//        System.out.println( hansu2(n) );
+//        System.out.println( hansu3(n) );
 
         bf.close();
     }
