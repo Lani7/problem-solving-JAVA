8be05d4168770a01e504c8b980a9e391b2ad4159
Lani7 <appletree0272@gmail.com>
2023-01-02 21:45:36 +0900

    18108

---
+++ src/main/java/com/lani/boj/Q18108.java
@@ -0,0 +1,17 @@
+package com.lani.boj;
+
+import java.util.Scanner;
+
+public class Q18108 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int bulgi = scanner.nextInt();
+
+        int adYear = bulgi - 543;
+
+        System.out.println(adYear);
+
+        scanner.close();
+    }
+}
