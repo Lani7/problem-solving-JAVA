794803b9ddb6bef48551d5dcd0169273360ca012
Lani7 <appletree0272@gmail.com>
2023-01-09 17:15:39 +0900

    2566

---
+++ src/main/java/com/lani/boj/Q2566.java
@@ -0,0 +1,33 @@
+package com.lani.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q2566 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int[][] arr = new int[9][9];
+
+        int max = 0;
+        int row = 1;
+        int col = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(arr[i][j] > max) {
+                    max = arr[i][j];
+                    row = i+1;
+                    col = j+1;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(row + " "+ col);
+        bf.close();
+    }
+}
