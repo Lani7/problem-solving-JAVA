02c99ee98aeb9ca0253de92d06744eaca4447b15
Lani7 <appletree0272@gmail.com>
2023-02-25 21:35:06 +0900

    10813

---
+++ src/main/java/com/lani/boj/Q10813.java
@@ -0,0 +1,38 @@
+package com.lani.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q10813 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n+1];
+
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = i;
+        }
+
+        for (int a = 0; a < m; a++) {
+            String[] s = bf.readLine().split(" ");
+            int i = Integer.parseInt(s[0]);
+            int j = Integer.parseInt(s[1]);
+            int temp = 0;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        for (int i = 1; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        bf.close();
+    }
+}
