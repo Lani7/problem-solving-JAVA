79466bd779de6ca8888df607c53ea0161d6d9b84
Lani7 <appletree0272@gmail.com>
2023-02-27 23:27:29 +0900

    Q2743

---
+++ src/main/java/com/lani/boj/Q2743.java
@@ -0,0 +1,16 @@
+package com.lani.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(bf.readLine().length());
+
+        bf.close();
+
+    }
+}
