3e374667e933d62e8f027618b1ce535db2145bd1
Lani7 <appletree0272@gmail.com>
2023-03-09 13:19:10 +0900

    10812

--- src/main/java/com/lani/boj/Q10812.java
+++ src/main/java/com/lani/boj/Q10812.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Q10812 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
@@ -17,28 +17,36 @@ public class Main {
             basket[i] = i;
         }
 
+        int[] rotated = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            rotated[i] = i;
+        }
+
         for (int l = 0; l < m; l++) {
             StringTokenizer s = new StringTokenizer(bf.readLine(), " ");
             int i = Integer.parseInt(s.nextToken());
             int j = Integer.parseInt(s.nextToken());
             int k = Integer.parseInt(s.nextToken());
 
-            for (int o = i; o <= j; o++) {
-                 int temp = basket[i];
-                 basket[i] = basket[k];
-                 basket[k+o] = temp;
+            int d = (j - k) + 1;    // k부터 end인 j까지의 차이
+
+            // k부터 end인 j까지의 숫자를 rotated 배열에 넣는다
+            for (int o = k, q = i; o <= j; o++, q++) {
+                rotated[q] = basket[o];
+            }
+            // 처음begin부터 k 이전까지의 숫자를 rotated 배열에 넣는다
+            for (int o = i; o < k; o++) {
+                rotated[d + o] = basket[o];
+            }
+            // rotated 배열을 basket 배열로 옮긴다
+            for (int o = 1; o <= j; o++) {
+                basket[o] = rotated[o];
             }
         }
 
-        for (int el: basket) {
-            System.out.print(el + " ");
+        for (int i = 1; i < rotated.length; i++) {
+            System.out.print(rotated[i] + " ");
         }
-
-
-
-
-
-
 
         bf.close();
     }
