1fb8e08bd5898fa8d0bb011fbf2c5bf57b98dad9
Lani7 <appletree0272@gmail.com>
2023-11-18 20:24:52 +0900

    수 정렬하기 5

--- src/main/java/com/lani/boj/silver/S15688.java
+++ src/main/java/com/lani/boj/silver/S15688.java
@@ -8,18 +8,13 @@ public class S15688 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-//        List<Integer> list = new ArrayList<>();
         StringBuffer sb = new StringBuffer();
 
         for (int i = 0; i < n; i++)
-//            list.add(Integer.parseInt(br.readLine()));
             arr[i] = Integer.parseInt(br.readLine());
 
-//        Collections.sort(list);
         Arrays.sort(arr);
-//        Arrays.sort(arr, (num1,num2)->num2-num1);
 
-//        for (int i: list)
         for (int i: arr)
             sb.append(i).append('\n');
 
